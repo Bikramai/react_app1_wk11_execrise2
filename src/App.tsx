70e5d00f7a296d11a65a0c277bb29c1096d4b34a
bikramia <bikram.rhce@gmail.com>
2023-12-24 07:50:55 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+import Like from "./components/Like";
+
+
+function App() {
+  return (
+      <div>
+        <Like onClick={() => console.log('clicked')}/>
+      </div>
+    );
+  }
+
+
+export default App;
